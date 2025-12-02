4a9cd13aba9ee2870d929fc9fe3b82a05bd46d07
artem <artem@LERNER>
2025-12-02 13:04:34 +0200

    Add project files.

---
+++ 12lr/12lr.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip> 
+#include <string.h> 
+
+using namespace std;
+
+int main()
+{
+    char s1[20] = "Hello World";
+    char s2[20] = "Hello";
+
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+    
+    for (int i = 0; i <= len1 - len2; i++) {
+        if (strncmp(s1 + i, s2, len2) == 0)
+        {
+            int pos = 1;
+        }
+        else {
+            cout << s1;
+            return 0;
+        }
+    }
+   
+
+}
