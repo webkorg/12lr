4df6ab33e586f96aa1f2427f78fe089fa49aef53
webkorg <web.korg@gmail.com>
2025-12-02 13:57:49 +0200

    add new file

---
+++ 11лр.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{
+    const int n = 3;
+    const int m = 3;
+    int matrix[n][m] = {
+        {3, 4, 5},
+        {4, 5, 6},
+        {3, 2, 6}
+    };
+    for (int j = 0; j < n; j++){
+
+        for (int i = 0; i < m; i++)
+        {
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
+    for (int j = 0; j < m; j++) {
+        int minRow = 0, maxRow = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (matrix[i][j] < matrix[minRow][j]) minRow = i;
+            if (matrix[i][j] > matrix[maxRow][j]) maxRow = i;
+        }
+        matrix[minRow][j] = matrix[maxRow][j];
+    }
+    cout << endl;
+
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++)
+        {
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+
